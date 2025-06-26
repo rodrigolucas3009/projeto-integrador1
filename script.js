36d86d14e39d78d7ac0024aada559ee13aa3fa0e
rodrigolucas3009 <rodrigolmmoreiraleao@gmail.com>
2025-06-26 08:40:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 document.addEventListener("DOMContentLoaded", function () {
     const botoes = document.querySelectorAll(".botao-curso");
 
-    botoes.forEach(botao => {
-        botao.addEventListener("mouseover", () => {
+    botoes.forEach(function (botao) {
+        botao.addEventListener("mouseover", function () {
             botao.style.transform = "scale(1.1)";
-            botao.style.transition = "transform 0.3s";
         });
 
-        botao.addEventListener("mouseout", () => {
+        botao.addEventListener("mouseout", function () {
             botao.style.transform = "scale(1)";
+        });
+
+        botao.addEventListener("click", function (e) {
+            alert(`Você clicou em ${botao.textContent}!`);
         });
     });
 });
